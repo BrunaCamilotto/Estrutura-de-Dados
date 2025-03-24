08c1e0ceb8a375165b4a2407b7d7945ba5d1f75e
Bruna Camilotto <92326961+BrunaCamilotto@users.noreply.github.com>
2025-03-24 13:48:27 -0300

    exercício de funções 3

---
+++ exercicio-funcao-3/exercicio-funcao-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+double maiorValor(double x, double y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int main() {
+    double x, y;
+
+    printf("Insira dois valores separados por um unico espaco (X e Y): ");
+    scanf("%lf %lf", &x, &y);
+
+    printf("O maior valor eh: %.2f\n", maiorValor(x, y));
+
+    return 0;
+}
+
