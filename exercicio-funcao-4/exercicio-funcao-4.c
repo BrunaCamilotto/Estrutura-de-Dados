195eeb06fb792cc1efb3687187c722e49eb4227c
Bruna Camilotto <92326961+BrunaCamilotto@users.noreply.github.com>
2025-03-24 13:51:50 -0300

    exercício de funções 4

---
+++ exercicio-funcao-4/exercicio-funcao-4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int saoMultiplos(int a, int b) {
+    if (a % b == 0 || b % a == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    int a, b;
+
+    printf("Digite dois valores separados por uma unica barra de espaco: ");
+    scanf("%d %d", &a, &b);
+
+    if (saoMultiplos(a, b)) {
+        printf("Os numeros sao multiplos.\n");
+    } else {
+        printf("Os numeros nao sao multiplos.\n");
+    }
+
+    return 0;
+}
